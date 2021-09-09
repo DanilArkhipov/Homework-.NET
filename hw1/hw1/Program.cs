e540d64ff586c78c92d947d41698066b019eb8a6
LipatovAlexander <lipatov.work@bk.ru>
2021-09-09 16:33:55 +0300

    add calculator

--- hw1/hw1/Program.cs
+++ hw1/hw1/Program.cs
@@ -2,11 +2,34 @@
 
 namespace hw1
 {
-    class Program
+    internal class Program
     {
-        static void Main (string[] args)
+        private static void Main (string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var val1 = int.Parse(args[0]);
+            var operation = args[1];
+            var val2 = int.Parse(args[2]);
+
+            var result = 0;
+            switch (operation)
+            {
+                case "+":
+                    result = val1 + val2;
+                    break;
+
+                case "-":
+                    result = val1 - val2;
+                    break;
+
+                case "*":
+                    result = val1 * val2;
+                    break;
+
+                case "/":
+                    result = val1 / val2;
+                    break;
+            }
+            Console.WriteLine($"Result is {result}");
         }
     }
 }
