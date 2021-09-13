9fe259e94591838d3f77cbcaa9885b3df02e590f
LipatovAlexander <lipatov.work@bk.ru>
2021-09-13 22:14:04 +0300

    add tests

---
+++ hw1/hw1.Tests/Program.Test.cs
@@ -0,0 +1,20 @@
+﻿using hw1;
+using Xunit;
+
+namespace Tests
+{
+    public class ProgramTest
+    {
+        [Fact]
+        public void WhenArgsIncorrect()
+        {
+            Assert.False(Program.Main(new[]{"1", "+"}) == 0);
+        }
+
+        [Fact]
+        public void WhenArgsCorrect()
+        {
+            Assert.True(Program.Main(new[]{"1", "+", "2"}) == 0);
+        }
+    }
+}
