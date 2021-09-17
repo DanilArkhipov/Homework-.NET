a9d4e5ff7adcdece224af541dab619d586cf8f84
LipatovAlexander <lipatov.work@bk.ru>
2021-09-17 17:20:04 +0300

    update tests

--- hw1/hw1.Tests/Parser.Tests.cs
+++ hw1/hw1.Tests/Parser.Tests.cs
@@ -8,44 +8,46 @@ namespace Tests
         [Fact]
         public void WhenArgsLengthIncorrect()
         {
-            Check(new[]{"1", "+"}, Parser.WrongArgsLength);
-            Check(new[]{"1", "+", "2", "="}, Parser.WrongArgsLength);
+            Check(new[]{"1", "+"}, false, Parser.WrongArgsLength);
+            Check(new[]{"1", "+", "2", "="}, false, Parser.WrongArgsLength);
         }
 
         [Fact]
         public void WhenOperationIncorrect()
         {
-            Check(new[]{"1", "%", "2"}, Parser.WrongOperation);
+            Check(new[]{"1", "%", "2"}, false, Parser.WrongOperation);
         }
 
         [Fact]
         public void WhenArgsIncorrect()
         {
-            Check(new[]{"x", "+", "1"}, Parser.WrongArgFormat);
-            Check(new[]{"1", "+", "x"}, Parser.WrongArgFormat);
+            Check(new[]{"x", "+", "1"}, false, Parser.WrongArgFormat);
+            Check(new[]{"1", "+", "x"}, false, Parser.WrongArgFormat);
         }
 
         [Fact]
         public void WhenAllCorrect()
         {
-            Check(new[]{"1", "+", "2"}, Parser.AllCorrect, 1, Operation.Plus, 2);
-            Check(new[]{"1", "-", "2"}, Parser.AllCorrect, 1, Operation.Minus, 2);
-            Check(new[]{"1", "*", "2"}, Parser.AllCorrect, 1, Operation.Multiply, 2);
-            Check(new[]{"1", "/", "2"}, Parser.AllCorrect, 1, Operation.Divide, 2);
+            Check(new[]{"1", "+", "2"}, true, Parser.AllCorrect, 1, Operation.Plus, 2);
+            Check(new[]{"1", "-", "2"}, true, Parser.AllCorrect, 1, Operation.Minus, 2);
+            Check(new[]{"1", "*", "2"}, true, Parser.AllCorrect, 1, Operation.Multiply, 2);
+            Check(new[]{"1", "/", "2"}, true, Parser.AllCorrect, 1, Operation.Divide, 2);
         }
 
-        private void Check(string[] args, int expectedCode, int expectedVal1, Operation expectedOperation, int expectedVal2)
+        private void Check(string[] args, bool expectedResult, int expectedCode, int expectedVal1, Operation expectedOperation, int expectedVal2)
         {
-            var code = Parser.TryParseArgs(args, out var val1, out var operation, out var val2);
+            var result = Parser.TryParseArgs(args, out var val1, out var operation, out var val2, out var code);
+            Assert.Equal(expectedResult, result);
             Assert.Equal(expectedCode, code);
             Assert.Equal(expectedVal1, val1);
             Assert.Equal(expectedOperation, operation);
             Assert.Equal(expectedVal2, val2);
         }
 
-        private void Check(string[] args, int expectedCode)
+        private void Check(string[] args, bool expectedResult, int expectedCode)
         {
-            var code = Parser.TryParseArgs(args, out _, out _, out _);
+            var result = Parser.TryParseArgs(args, out _, out _, out _, out var code);
+            Assert.Equal(expectedResult, result);
             Assert.Equal(expectedCode, code);
         }
     }
