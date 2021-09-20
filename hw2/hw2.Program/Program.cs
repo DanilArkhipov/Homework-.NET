1275c4229b882b631c69057b4dec48545300be04
LipatovAlexander <lipatov.work@bk.ru>
2021-09-20 21:22:21 +0300

    initial commit
    add 2 projects

---
+++ hw2/hw2.Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hw2.Program
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Calculator.Square(5));
+        }
+    }
+}
