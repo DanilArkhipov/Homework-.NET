a50f267751c4d381db3326a22b27150e023bc417
LipatovAlexander <lipatov.work@bk.ru>
2021-09-21 18:52:46 +0300

    Add main method from hw1

--- hw2/hw2.Program/Program.cs
+++ hw2/hw2.Program/Program.cs
@@ -4,9 +4,21 @@ namespace hw2.Program
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            Console.WriteLine(Calculator.Square(5));
+            var argsAreCorrect = Parser.TryParseArgs(
+                args,
+                out var val1,
+                out var operation,
+                out var val2,
+                out var codeOfParsing);
+            if (argsAreCorrect)
+            {
+                var result = Calculator.Calculate(val1, operation, val2);
+                Console.WriteLine($"Result is {result}");
+            }
+
+            return codeOfParsing;
         }
     }
 }
